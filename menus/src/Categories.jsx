97109304b5b269a88e72883115c5ebf016314d09
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2023-02-14 17:49:59 +0100

    new change

---
+++ menus/src/Categories.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Categories(){
+    return(
+        <div>
+            <h4>this is my category</h4>
+        </div>
+    )
+}
+export default Categories
---
+++ menus/src/Menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Menu(){
+    return(
+        <div>
+         <h2>this is my menu area</h2>
+        </div>
+    )
+}
+
+export default Menu
